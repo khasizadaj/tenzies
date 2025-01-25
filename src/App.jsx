7e19ac92404deab1f2a5167653607663677cb076
jkhasizada <javid.khasizada@gmail.com>
2025-01-26 02:51:58 +0400

    Refactor Die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
-
-function Die(props) {
-  return (
-    <button
-      onClick={props.hold}
-      className={
-        `col-span-1 flex flex-col justify-center items-center text-center rounded-lg h-24 w-24 text-2xl font-bold text-blue-50` +
-        (props.isHeld
-          ? " bg-blue-950 border-4 border-blue-400 border-solid"
-          : " bg-blue-600")
-      }
-    >
-      {props.value}
-    </button>
-  );
-}
+import Die from "./components/Die";
 
 function generateNewDice() {
   const list = new Array(10).fill(0).map(() => {
