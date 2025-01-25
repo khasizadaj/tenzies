8b825438d676d6b50970a5b59069e88ca4b81cea
jkhasizada <javid.khasizada@gmail.com>
2025-01-25 20:52:33 +0400

    Add conditinal rendering for held die

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,12 @@ function Die(props) {
   return (
     <button
       onClick={handleClick}
-      className="col-span-1 flex flex-col justify-center items-center text-center bg-blue-950 rounded-lg h-24 w-24 text-2xl font-bold text-blue-50"
+      className={
+        `col-span-1 flex flex-col justify-center items-center text-center rounded-lg h-24 w-24 text-2xl font-bold text-blue-50` +
+        (props.isHeld
+          ? " bg-blue-950 border-4 border-blue-400 border-solid"
+          : " bg-blue-600")
+      }
     >
       {props.value}
     </button>
