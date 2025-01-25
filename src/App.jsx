d1ec407b830d43bbb9729ebd13ea4381da7148d2
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2025-01-25 22:28:59 +0400

    Add die and hold values (#1)
    * Add hold function & Update roll functionality
    * Add conditional rendering for held die
    * Create die object & add id for `key` parameter
    
    ## Code Quality
    
    * Use Array to generate list of number
    * Drop `useEffect`
    * Remove unused dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function Die(props) {
-  function handleClick() {
-    console.log(props.value);
-  }
-
   return (
-    <button onClick={handleClick} className="col-span-1 flex flex-col justify-center items-center text-center bg-blue-950 rounded-lg h-24 w-24 text-2xl font-bold text-blue-50">
+    <button
+      onClick={props.hold}
+      className={
+        `col-span-1 flex flex-col justify-center items-center text-center rounded-lg h-24 w-24 text-2xl font-bold text-blue-50` +
+        (props.isHeld
+          ? " bg-blue-950 border-4 border-blue-400 border-solid"
+          : " bg-blue-600")
+      }
+    >
       {props.value}
     </button>
   );
 }
 
-function getContent() {
-  const cardList = [];
-  for (let i = 0; i < 10; i++) {
-    cardList.push(Math.ceil(Math.random() * 6));
-  }
-  console.log(cardList);
-  return cardList;
+function generateNewDice() {
+  const list = new Array(10).fill(0).map(() => {
+    return {
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+    };
+  });
+  return list;
 }
 
 function App() {
-  const [contentList, setContentList] = useState([]);
+  const [dice, setDice] = useState(generateNewDice());
 
-  useEffect(() => {
-    setContentList(getContent());
-  }, []);
+  function roll() {
+    setDice((dice) =>
+      dice.map((die) => {
+        if (die.isHeld) {
+          return die;
+        }
+        return { ...die, value: Math.ceil(Math.random() * 6) };
+      })
+    );
+  }
 
-  function reset() {
-    setContentList(getContent());
+  function hold(id) {
+    setDice((dice) => {
+      return dice.map((die) => {
+        if (die.id === id) {
+          return { ...die, isHeld: !die.isHeld };
+        }
+        return die;
+      });
+    });
   }
 
   return (
@@ -43,16 +64,23 @@ function App() {
           </h3>
         </div>
         <div className="grid grid-cols-5 grid-row-span-2 gap-2 sm:gap-4 px-4 sm:px-12">
-          {contentList.map((value, index) => {
-            return <Die key={index} value={value} />;
+          {dice.map((die) => {
+            return (
+              <Die
+                key={die.id}
+                value={die.value}
+                isHeld={die.isHeld}
+                hold={() => hold(die.id)}
+              />
+            );
           })}
         </div>
         <div>
           <button
-            onClick={reset}
-            className="bg-blue-500 text-white p-4 text-xl font-bold rounded-lg m-8"
+            onClick={roll}
+            className="bg-blue-500 text-white p-4 px-8 text-xl font-bold rounded-lg m-8"
           >
-            Restart
+            Roll
           </button>
         </div>
       </section>
