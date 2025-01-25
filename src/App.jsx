7317e66f3ba925439d89c3ad7b4ec3f332e50fa4
jkhasizada <javid.khasizada@gmail.com>
2025-01-26 03:29:14 +0400

    And end game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
 import Die from "./components/Die";
+import { useWindowSize } from "react-use";
+import Confetti from "react-confetti";
 
 function generateNewDice() {
   const list = new Array(10).fill(0).map(() => {
@@ -14,7 +16,11 @@ function generateNewDice() {
 }
 
 function App() {
-  const [dice, setDice] = useState(generateNewDice());
+  const [dice, setDice] = useState(() => generateNewDice());
+  const gameIsOver = dice.every(
+    (die) => die.isHeld && die.value === dice[0].value
+  );
+  const { width, height } = useWindowSize();
 
   function roll() {
     setDice((dice) =>
@@ -22,9 +28,13 @@ function App() {
         if (die.isHeld) {
           return die;
         }
-        return { ...die, value: Math.ceil(Math.random() * 6) };
+        return { ...die, value: 1 };
       })
     );
+  }
+
+  function restart() {
+    setDice(generateNewDice());
   }
 
   function hold(id) {
@@ -40,6 +50,16 @@ function App() {
 
   return (
     <main>
+      {gameIsOver && (
+        <Confetti
+          width={width}
+          height={height}
+          recycle={false}
+          numberOfPieces={1000}
+          tweenDuration={7000}
+          gravity={0.1}
+        />
+      )}
       <section className="flex flex-col justify-center items-center gap-4 h-screen bg-blue-900">
         <div className="flex-grow-0 space-y-2 text-center p-4 sm:py-8">
           <h1 className="text-4xl font-bold text-blue-50">Tenzies</h1>
@@ -61,12 +81,24 @@ function App() {
           })}
         </div>
         <div>
-          <button
-            onClick={roll}
-            className="bg-blue-500 text-white p-4 px-8 text-xl font-bold rounded-lg m-8"
-          >
-            Roll
-          </button>
+          {gameIsOver ? (
+            <button
+              onClick={restart}
+              className={
+                `bg-blue-500 text-white p-4 px-8 text-xl font-bold rounded-lg m-8` +
+                (gameIsOver ? " bg-blue-50 text-blue-900" : "")
+              }
+            >
+              New Game
+            </button>
+          ) : (
+            <button
+              onClick={roll}
+              className="bg-blue-500 text-white p-4 px-8 text-xl font-bold rounded-lg m-8"
+            >
+              Roll
+            </button>
+          )}
         </div>
       </section>
     </main>
